c8348bb88686230d2db0c64b6472b19d9c99266b
Zamayaka <zamayaka2013@gmail.com>
2017-11-04 10:47:32 +0200

    english text

--- OOP-HW4_Interfaces/Main.java
+++ OOP-HW4_Interfaces/Main.java
@@ -4,14 +4,14 @@ public class Main {
 
 	public static void main(String[] args) {
 		Group gr1 = new Group();
-		// Наполнение Группы
+		// Filling a group of students
 		try {
 			gr1.addToGroup(new Student("Mihailova", "Lena", 20, false, "OB03G", 103));
 			gr1.addToGroup(new Student("Romanoff", "Pizza", 21, false, "OB02G", 54));
 			gr1.addToGroup(new Student("Barentsev", "Vovan", 22, true, "1OB04G", 10));
 			gr1.addToGroup(new Student("Glushkov", "Zheka", 18, true, "2OB04G", 23));
 			gr1.addToGroup(new Student("Yatseniuk", "KuliaVLoba", 20, true, "3OB04G", 71));
-			// Интерактивное добавление студента в группу
+			// Interactive adding student to group
 			//gr1.addToGroupInteractive();
 		} catch (MyException e) {
 			System.out.println(e.getMessage());
@@ -19,15 +19,15 @@ public class Main {
 
 		System.out.println("Group contains:\n"+gr1);
 
-		// Поиск по Фамилии
-		// System.out.println("Поиск по фамилии 'Barentsev'\n" +
+		// Search by Last Name
+		// System.out.println("Search by Last Name 'Barentsev'\n" +
 		// gr1.getStudent("Barentsev"));
 
-		// Удаление из группы по номеру
+		// Delete from group by number
 		//	gr1.delFromGroup(2);
 
-		System.out.println("Интерактивная Сортировка по параметру\n"+gr1.sortParametr());
-		System.out.println("На службу идут\n"+new Group(gr1.naSluzhbu()));
+		System.out.println("Interactive SORT by Parametr\n"+gr1.sortParametr());
+		System.out.println("Going to Army\n"+new Group(gr1.naSluzhbu()));
 
 	}
 
